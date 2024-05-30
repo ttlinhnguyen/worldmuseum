47517e235645a9458e7ad8eed493af8e94c4c0f4
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-05-30 21:27:03 +0930

    Use separate class for model to prevent invalid url error

--- src/app/components/Model3D.tsx
+++ src/app/components/Model3D.tsx
@@ -3,11 +3,15 @@
 import { Canvas } from "@react-three/fiber";
 import { useGLTF, OrbitControls } from "@react-three/drei";
 export default function Model3D() {
-    const model = useGLTF("/cesar_louvre_museum/scene.gltf");
     return (
         <Canvas shadows camera={{ position: [10, 4, 0], fov: 4 }}>
-            <primitive object={model.scene} position={[0, -0.2, 0]} />
+            <Model />
             <OrbitControls makeDefault />
         </Canvas>
     );
+}
+
+function Model() {
+    const model = useGLTF("/cesar_louvre_museum/scene.gltf");
+    return <primitive object={model.scene} position={[0, -0.2, 0]} />;
 }
