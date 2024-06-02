7c980c34883dde182157ff13ad78eb8db1575d91
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-06-02 21:28:56 +0930

    Add more text content for SolutionDetails

--- src/app/components/SolutionDetails.tsx
+++ src/app/components/SolutionDetails.tsx
@@ -19,8 +19,18 @@ export default function SolutionDetails() {
                 ></div>
             ),
             title: "3D Modelling",
+            subtitle: "Explore every intricate detail from any angle",
             description: (
                 <div>
+                    <div className="mb-3">
+                        3D Modelling brings artefacts to life in a digital
+                        space, enhancing the accessibility and interactivity of
+                        our collections and reducing physical handling of
+                        fragile items. With 3D models, we can create immersive
+                        virtual tours and educational tools that provide a
+                        deeper understanding of cultural artefacts, making
+                        history more engaging and accessible to everyone.
+                    </div>
                     <PopUp text="Show 3D Model">
                         <div className="">
                             <h3>3D Model</h3>
@@ -59,7 +69,18 @@ export default function SolutionDetails() {
                 ></div>
             ),
             title: "AI Recognition",
-            description: "Lorem ipsum",
+            subtitle: "Gain new insights into our collections",
+            description: (
+                <div>
+                    AI recognition transforms how we catalog and understand our
+                    extensive collections. This technology swiftly and
+                    accurately classifies artefacts and interprets historical
+                    texts, providing valuable insights that might otherwise be
+                    missed. By leveraging AI recognition, we enhance the
+                    museum's effiency and deepen out understanding of each
+                    artefact's historical and cultural significance.
+                </div>
+            ),
         },
         {
             id: "rfid-tracking",
@@ -70,11 +91,23 @@ export default function SolutionDetails() {
                 />
             ),
             title: "RFID Tracking",
-            description: "Lorem ipsum",
+            subtitle: "Protect and manage our cultural treasures",
+            description: (
+                <div>
+                    RFID (Radio Frequency Identification) revolutionises the way
+                    we monitor and manage our 25+ million objects. This tracking
+                    system provides real-time updates on the location of
+                    artefacts, significantly reducing the risk of theft and
+                    misplacement. By implementing RFID tracking, we uphold the
+                    integrity of our collections, offering peace of mind to
+                    stakeholders, and ensuring our cultural treasures are
+                    protected for future generations.
+                </div>
+            ),
         },
     ];
     return (
-        <div className="my-5">
+        <div className="m-5">
             <FadeInDiv className="container sm-container text-center">
                 <h2 className="py-10">
                     Transform your museum experience with cutting-edge
@@ -83,27 +116,35 @@ export default function SolutionDetails() {
             </FadeInDiv>
             {DETAILS.map((item, index) => {
                 return (
-                    <div
+                    <FadeInDiv
                         key={index}
                         id={item.id}
-                        className={`container px-5 py-10 justify-center flex flex-wrap gap-10 ${
-                            index % 2 == 0 ? "flex-row" : "flex-row-reverse"
+                        className={`container my-10 justify-between flex bg-white shadow flex-col ${
+                            index % 2 == 0
+                                ? "md:flex-row"
+                                : "md:flex-row-reverse"
                         }`}
                     >
-                        <FadeInDiv className="w-screen md:w-96">
+                        <div className="w-full md:w-96 flex-none">
                             <div
-                                className="place-content-center rounded-lg drop-shadow bg-gradient-to-b from-slate-950 to-slate-700 relative w-full h-96"
-                                // style={{ height: "30rem" }}
+                                className="place-content-center drop-shadow bg-gradient-to-b from-slate-950 to-slate-700 relative w-full"
+                                style={{ height: "30rem" }}
                             >
                                 {item.media}
                             </div>
                             <div className="text-xs">{item.credit}</div>
-                        </FadeInDiv>
-                        <FadeInDiv className="flex-auto" delay={0.6}>
-                            <h3 className="pb-5">{item.title}</h3>
-                            <div>{item.description}</div>
-                        </FadeInDiv>
-                    </div>
+                        </div>
+                        <div className="py-5 px-10">
+                            <h3 className="">{item.title}</h3>
+                            <div>
+                                <i>{item.subtitle}</i>
+                            </div>
+                            <hr />
+                            <div className="text-gray-600">
+                                {item.description}
+                            </div>
+                        </div>
+                    </FadeInDiv>
                 );
             })}
         </div>
