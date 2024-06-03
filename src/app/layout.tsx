48ee0310936cd08f81beaf874a3ac9b0005e9692
Linh Nguyen <linhtunguyen.1311@gmail.com>
2024-06-03 20:45:13 +0930

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,12 +3,30 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Script from "next/script";
+import ROOT_DIR from "./data/root";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const TITLE = "World Museum";
+const DESCRIPTION = "Digitisation of World Museum";
 export const metadata: Metadata = {
-    title: "World Museum",
-    description: "Digitisation of World Museum",
+    title: TITLE,
+    description: DESCRIPTION,
+    icons: {
+        icon: `${ROOT_DIR}/favicon/favicon.ico`,
+        apple: `${ROOT_DIR}/favicon/apple-touch-icon.png`,
+    },
+    manifest: `${ROOT_DIR}/favicon/site.webmanifest`,
+    openGraph: {
+        title: TITLE,
+        description: DESCRIPTION,
+        siteName: TITLE,
+        images: [
+            {
+                url: "https://images.unsplash.com/photo-1451187580459-43490279c0fa",
+            },
+        ],
+    },
 };
 
 export default function RootLayout({
